ee2fe3d6eb0c2627554309e47dfae525edf8b1ff
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2024-01-31 09:48:53 -0300

    login page

--- lab_clinicas_self_services/lib/main.dart
+++ lab_clinicas_self_services/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
 import 'package:lab_clinicas_self_services/src/binding/lab_clinica_application_bindings.dart';
+import 'package:lab_clinicas_self_services/src/modules/auth/auth_module.dart';
 import 'package:lab_clinicas_self_services/src/pages/splash_pages/splash_page.dart';
 
 void main() {
@@ -21,6 +22,9 @@ class LabClinicaSelfService extends StatelessWidget {
           page: (context) => const SplashPage(),
           path: '/',
         )
+      ],
+      modules: [
+        AuthModule(),
       ],
     );
   }
--- lab_clinicas_self_services/lib/src/modules/auth/auth_module.dart
+++ lab_clinicas_self_services/lib/src/modules/auth/auth_module.dart
@@ -1,1 +1,22 @@
-class AuthModule {}
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:lab_clinicas_self_services/src/modules/auth/login/login_router.dart';
+import 'package:lab_clinicas_self_services/src/repositories/user/user_repository.dart';
+import 'package:lab_clinicas_self_services/src/repositories/user/user_repository_impl.dart';
+
+class AuthModule extends FlutterGetItModule {
+  @override
+  String get moduleRouteName => '/auth';
+
+  @override
+  List<Bind<Object>> get bindings => [
+        Bind.lazySingleton<UserRepository>(
+          (i) => UserRepositoryImpl(restClient: i()),
+        )
+      ];
+
+  @override
+  Map<String, WidgetBuilder> get pages => {
+        '/login': (_) => const LoginRouter(),
+      };
+}
